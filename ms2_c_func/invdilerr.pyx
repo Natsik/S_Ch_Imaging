8aadc3785d5694870ed0f1aecd30bd9946fa8334
Natsik <nats08spb@yandex.ru>
2014-03-01 00:28:55 +0400

    c realizations of ms2

---
+++ ms2_c_func/invdilerr.pyx
@@ -0,0 +1,27 @@
+import numpy as np
+cimport numpy as np
+
+np.import_array()
+
+cdef extern from "invdilerr.h":
+	void c_inversion(const unsigned char * src, unsigned char * dst, int channelStride, int h)
+	void c_dilatation(const unsigned char * src, unsigned char * dst, int channelStride, int h)
+	void c_errosion(const unsigned char * src, unsigned char * dst, int channelStride, int h)
+
+def c_inversion_func(np.ndarray[char, ndim=1, mode="c"] in_array not None,
+                     np.ndarray[char, ndim=1, mode="c"] out_array not None):
+    c_inversion(<unsigned char*> np.PyArray_DATA(in_array),
+                <unsigned char*> np.PyArray_DATA(out_array),
+                in_array.shape[0])
+
+def c_dilatation_func(np.ndarray[char, ndim=1, mode="c"] in_array not None,
+                     np.ndarray[char, ndim=1, mode="c"] out_array not None):
+    c_dilatation(<unsigned char*> np.PyArray_DATA(in_array),
+                <unsigned char*> np.PyArray_DATA(out_array),
+                in_array.shape[0])
+
+def c_errosion_func(np.ndarray[char, ndim=1, mode="c"] in_array not None,
+                     np.ndarray[char, ndim=1, mode="c"] out_array not None):
+    c_errosion(<unsigned char*> np.PyArray_DATA(in_array),
+                <unsigned char*> np.PyArray_DATA(out_array),
+                in_array.shape[0])
