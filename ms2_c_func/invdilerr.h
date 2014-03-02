f91c2980cf04972c6e0a99398254b77b964a5e60
Natsik <nats08spb@yandex.ru>
2014-03-02 23:26:20 +0400

    code refactoring

--- ms2_c_func/invdilerr.h
+++ ms2_c_func/invdilerr.h
@@ -1,4 +1,4 @@
-void c_inversion(const unsigned char * src, unsigned char * dst, int w, int h);
+void c_inversion(const unsigned char * src, unsigned char * dst, int channelStride, int h);
 void c_dilatation(const unsigned char * src, unsigned char * dst, int channelStride, int h);
 void c_errosion(const unsigned char * src, unsigned char * dst, int channelStride, int h);
 //example if image depth is 24 (R, G, B) and width is 100 then channelStride == 300
