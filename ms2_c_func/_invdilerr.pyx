a6900b6c201cafba6a3764210f42874a572a13f5
Natsik <nats08spb@yandex.ru>
2014-03-02 23:26:00 +0400

    adjustments in variable names

--- ms2_c_func/_invdilerr.pyx
+++ ms2_c_func/_invdilerr.pyx
@@ -4,16 +4,16 @@ cimport numpy as np
 np.import_array()
 
 cdef extern from "invdilerr.h":
-    void c_inversion(const unsigned char *src, unsigned char *dst, int w, int h)
+    void c_inversion(const unsigned char *src, unsigned char *dst, int channelStride, int h)
     void c_dilatation(const unsigned char *src, unsigned char *dst, int channelStride, int h)
     void c_errosion(const unsigned char *src, unsigned char *dst, int channelStride, int h)
 
 def c_inversion_func(np.ndarray[char, ndim=1, mode="c"] in_array not None,
                      np.ndarray[char, ndim=1, mode="c"] out_array not None,
-                     w, h):
+                     channel_strides, h):
     c_inversion(<unsigned char*> np.PyArray_DATA(in_array),
                 <unsigned char*> np.PyArray_DATA(out_array),
-                w, h)
+                channel_strides, h)
 
 def c_dilatation_func(np.ndarray[char, ndim=1, mode="c"] in_array not None,
                       np.ndarray[char, ndim=1, mode="c"] out_array not None,
